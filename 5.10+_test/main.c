2c413b5180829a53c3afec82218acf296de87ba9
HenryM975 <henry975@yandex.ru>
2021-04-27 08:36:08 +0300

    ...

---
+++ 5.10+_test/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char testch[100] = "ijiojijiiojikioj  iojijsesefsef";
+int main()
+{
+    for(int i = 0; i < 20; i++)
+    {
+        printf("%c\n", testch[i]);
+    }
+    return 0;
+}
